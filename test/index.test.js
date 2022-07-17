ddcba9aabb68ac7aee2e517e6b8f92c0467e1321
Thotino GOBIN-GANSOU <gthomario@yahoo.com>
2022-07-17 10:47:33 +0200

    test: add a test scenarion

--- test/index.test.js
+++ test/index.test.js
@@ -1,9 +1,15 @@
 'use strict'
 
-const parsingModule = require('../index')
+const parsingModule = require('../index').parseFile
 const path = require('path')
 
 const fileAbsolutepath = path.resolve(__dirname, 'data/TechCrunchcontinentalUSA.csv')
 
-const data = await parsingModule.parseFile.parseFile(fileAbsolutepath)
-console.log({ data })
+describe('[unit] test support domain', () => {
+  test('Everything fine here', async () => {
+    const data = await parsingModule.parseFile(fileAbsolutepath, ',')
+    console.log(data)
+    expect(data).not.toBeNull()
+    expect(data).toBeInstanceOf(Array)
+  })
+})
