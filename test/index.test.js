ed846f53363aa60119f3de12182b601efe9234e2
Thotino GOBIN-GANSOU <gthomario@yahoo.com>
2022-07-18 05:23:45 +0200

    test: test scenario

--- test/index.test.js
+++ test/index.test.js
@@ -7,9 +7,12 @@ const fileAbsolutepath = path.resolve(__dirname, 'data/TechCrunchcontinentalUSA.
 
 describe('[unit] test support domain', () => {
   test('Everything fine here', async () => {
+    await expect(parsingModule.parseFile(null)).rejects.toThrow('ERR_NO_FILE_PATH_PROVIDED')    
+  })
+  test.only('Everything fine here', async () => {
     const data = await parsingModule.parseFile(fileAbsolutepath, ',')
-    console.log(data)
     expect(data).not.toBeNull()
     expect(data).toBeInstanceOf(Array)
+    expect(data).toHaveLength(1460)
   })
 })
